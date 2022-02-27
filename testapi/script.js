d4af2bef7bdd555d795e9c3f41805dc2903493d6
lananh <nguyenthilananh0606@gmail.com>
2022-02-27 23:44:03 +0700

    update test api project

--- testapi/script.js
+++ testapi/script.js
@@ -1,31 +1,96 @@
-var request = new XMLHttpRequest()
-// function getData() {
-//     const response = await fetch('https://ghibliapi.herokuapp.com/films')
-//     const data = await response.json()
-//   }
-    request.open('GET', 'https://gorest.co.in/public/v2/users', true)
-    request.onload = function () {
-        // Json-> Javascript object
-        var user = JSON.parse(this.response)
-        try{
-            if (200 == request.status) {
-                const card = document.createElement('div')
-                card.setAttribute('class', 'card')
+fetch("https://gorest.co.in/public/v2/users")
+	// Converting received data to JSON
+	.then(response => response.json())
+	.then(json => {
 
-                const h1 = document.createElement('h1')
-                h1.textContent = user.title
-
-                const p = document.createElement('p')
-                user.name = user.name
-
-                container.appendChild(card)
-                card.appendChild(h1)
-                card.appendChild(p)
+		let li = `<tr><thead><th>ID</th><th>Name</th><th>Email</th><th>Gender</th><th>Status</th><th>Actions</th></thead></tr>`;
+	
+		json.forEach(user => {
+			li += `<tr>
+                        <td>${user.id} </td>
+                        <td>${user.name}</td>
+                        <td>${user.email} </td>
+                        <td>${user.gender}</td>
+                        <td>${user.status} </td>
+                        <td ><button class="btn btn-danger" onClick="deleteUser(${user.id})"><i class="fa-solid fa-trash-can"></i></button></td>
+			        </tr>`;
+            
+		});
+    
+	document.getElementById("users").innerHTML = li;
+});
+//---------------------------------------------------------------
+const serialize= (form) => {
+    var field, s = [];
+    if (typeof form == 'object' && form.nodeName == "FORM") {
+        var len = form.elements.length;
+        for (i=0; i<len; i++) {
+            field = form.elements[i];
+            if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+                if (field.type == 'select-multiple') {
+                    for (j=form.elements[i].options.length-1; j>=0; j--) {
+                        if(field.options[j].selected)
+                            s[s.length] = "\"" +encodeURIComponent(field.name) + "\"" + ":" + "\""+ encodeURIComponent(field.options[j].value) + "\"";
+                    }
+                } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+                    s[s.length] = "\"" + encodeURIComponent(field.name) + "\"" + ":" + "\"" + encodeURIComponent(field.value)+ "\"";
+                }
             }
         }
-        catch(error){
-            console.log(error)
-        }
     }
+    return s.join(',').replace(/%20/g, '+');
+}
+//---------------------------------------------------
+const deleteUser = async(id)=>{
+    const data={};
+    fetch('https://gorest.co.in/public/v2/users/'+id, {
+        method: 'DELETE', 
+        mode: 'cors',
+        headers: {
+          'Authorization': 'Bearer 0d623a946175880564364c410ce6c7f5ec6b68cd27722d1e3d7ad11fb96ff916',
+          'Content-Type': 'application/json'
+        },
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin" : "*", 
+        "Access-Control-Allow-Credentials" : true,
+        body: JSON.stringify(data),
+      })
+      .then(
+          response => { alert('Deleted user ' + id);}
+      )
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+}
 
-    request.send()
+
+//---------------------------------------------------------------
+const addUser = (form) =>{
+    console.log(serialize(form));
+    console.log("hih");
+// const data = { name: 'example',
+// email: 'test1@gmail.com',
+// gender: 'male',
+// status: 'active',
+//  };
+
+// fetch('https://gorest.co.in/public/v2/users/4185', {
+//   method: 'POST', 
+//   cache: 'no-cache', 
+//   credentials: 'same-origin', 
+//   headers: {
+//     'Authorization': 'Bearer 0d623a946175880564364c410ce6c7f5ec6b68cd27722d1e3d7ad11fb96ff916',
+//     'Content-Type': 'application/json'
+//   },
+//   redirect: 'follow',
+//   body: JSON.stringify(data),
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Created new user:', data);
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+}
