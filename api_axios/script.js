e131d9e077cd7eac6c2840c0fe7e7579dc3a0382
lananh <nguyenthilananh0606@gmail.com>
2022-03-01 01:57:05 +0700

    update api_axios

--- api_axios/script.js
+++ api_axios/script.js
@@ -5,23 +5,24 @@ const  callAPI = async(method, url, payload) =>{
         method: method,
         url: url,
         headers: { 
-            'Authorization': 'Bearer 0d623a946175880564364c410ce6c7f5ec6b68cd27722d1e3d7ad11fb96ff916',
-            'Content-Type': 'application/json'
+            // 'Authorization': '',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
         }
     }
     if(method =='post'){
-        await axios.post(config, payload)
-        .then(response => {
-            console.log(response.data);
+        return await axios.post(config, payload)
+        .then(async (response) => {
+            console.log(response.data)
           })
           .catch(error => {
             console.log(error);
           })
     }
     else{
-        await axios(config)
+        return await axios(config)
         .then(async (response) => {
-            console.log(response.data)
             return response.data
           })
           .catch(error => {
@@ -34,43 +35,54 @@ const  callAPI = async(method, url, payload) =>{
 
 //----GET ALL USER
 async function getAllUser() {
-    await callAPI('get','https://gorest.co.in/public/v2/users', '').then(data => {
-        console.log(data)    
-    })
-    .catch(err => console.log(err))
+    try {
+        const response = await callAPI('get','https://reqres.in/api/users', '')
+        return response.data
+      }
+    catch (error) {
+        console.log(error);
+    }
+    // await callAPI('get','https://gorest.co.in/public/v2/users', '').then(data => {
+            // return data
+    // })
+    // .catch(err => console.log(err))
 }
 
 //----GET USER BY ID
 async function getUserById(id) {
-    await callAPI('get','https://gorest.co.in/public/v2/users'+'/'+ id, '').then(data => {
-        return data;
-    })
-    .catch(err => console.log(err))
+    try {
+        const response = await callAPI('get','https://reqres.in/api/users/'+ id.toString(), '')
+        console.log(response.data)
+      }
+    catch (error) {
+        console.log(error);
+    }
+    
     
 }
 
 //----DELETE USER BY ID
 async function deleteUser(id) {
     try {
-        const response = await callAPI('delete','https://gorest.co.in/public/v2/users'+'/'+ id, '')
-        console.log(response)
+        await callAPI('delele','https://reqres.in/api/users/'+ id.toString(), '')
+        console.log("Deleted user " + id + "::::" +response.data)
       }
     catch (error) {
         console.log(error);
     }
 }
 
-async function addUser(id, payload) {
-    await callAPI('post', 'https://gorest.co.in/public/v2/users' + '/'+ id, payload).then(data => {
-        console.log('created');
+async function addUser(payload) {
+    await callAPI('post', 'https://reqres.in/api/users', payload).then(data => {
+        console.log('RESPONSE:::'+ data);
     })
     .catch(err => console.log(err))
     
 }
 
 async function updateUser(id, payload) {
-    await callAPI('post', 'patch', 'https://gorest.co.in/public/v2/users' + '/'+ id, payload).then(data => {
-        console.log('updated');
+    await callAPI('put', 'https://reqres.in/api/users/' + id.toString(), payload).then(data => {
+        console.log('updated:::'+ data);
     })
     .catch(err => console.log(err))
     
@@ -79,20 +91,28 @@ async function updateUser(id, payload) {
 
 window.addEventListener('load', async () => {
     const listUser = await getAllUser();
-     console.log(listUser)
-    // let li = `<tr><thead><th>ID</th><th>Name</th><th>Email</th><th>Gender</th><th>Status</th><th>Actions</th></thead></tr>`;
+    let table_users = `<tr><thead><th>ID</th><th>First Name</th><th>Last name</th><th>Email</th></thead></tr>`;
 	
-	// 	listUser.forEach(user => {
-	// 		li += `<tr>
-    //                     <td>${user.id} </td>
-    //                     <td>${user.name}</td>
-    //                     <td>${user.email} </td>
-    //                     <td>${user.gender}</td>
-    //                     <td>${user.status} </td>
-    //                     <td ><button class="btn btn-danger" onClick="deleteUser(${user.id})"><i class="fa-solid fa-trash-can"></i></button></td>
-	// 		        </tr>`;
+		listUser.forEach(user => {
+			table_users += `<tr>
+                        <td>${user.id} </td>
+                        <td>${user.first_name}</td>
+                        <td>${user.last_name} </td>
+                        <td>${user.email}</td>
+			        </tr>`;
             
-	// 	});
+		});
     
-	// document.getElementById("users").innerHTML = li;
+	document.getElementById("users").innerHTML = table_users;
   });
+
+
+  let payload={
+    "name": "test",
+    "job": "test"
+}
+//   getUserById(2);
+//   addUser(payload);
+//   updateUser(2, payload);
+  deleteUser(2)
+
