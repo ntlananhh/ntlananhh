49212aae63c1a437e10b286a5f209a3de3fddb96
lananh <lananh.nguyen@nfq.asia>
2022-03-06 23:34:55 +0700

    Complete TodoApp 06/03/2022

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import Form from './components/Form.js';
+import TodoList from './components/TodoList';
 function App() {
+  const [inputText, setInputText]= useState("");
+  const [todos, setTodos]= useState([]);
+  const [status, setStatus]= useState("all");
+  const [filterTodos, setFilterTodos]= useState([]);
+
+  const filterHandler =() =>{
+    
+    switch(status){
+      case 'completed':
+        setFilterTodos(todos.filter(todo=> todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilterTodos(todos.filter(todo=> todo.completed === false));
+        break;
+      default:
+        setFilterTodos(todos);
+        break;
+      }
+  }
+
+  //--------useEffect
+  useEffect(()=>{
+    filterHandler();
+  }, [todos, status]);
   return (
     <div className="App">
-      
+      <header>Todo App </header>
+      <Form 
+          inputText={inputText}
+          todos={todos}
+          setTodos={setTodos}
+          setInputText={setInputText}
+          setStatus= {setStatus}
+      />
+
+      <TodoList 
+          todos={todos}
+          setTodos={setTodos}
+          filterTodos={filterTodos}
+      />
     </div>
   );
 }
